66d402f6c023d98df2351a8d70e0083ed52ffa03
Jonathan Huang <grokut@gmail.com>
2015-06-14 12:59:10 -0700

    add/ create addChild and build method to FamilyTree class
    The addChild method takes a FamilyTree and adds it to the children array of the parent.
    
    The build method takes a json object that maps a family-tree and builds a family-tree from it.

--- spec/FamilyTreeSpec.js
+++ spec/FamilyTreeSpec.js
@@ -1,35 +1,118 @@
 'use strict';
 
 var FamilyTree = require('../src/FamilyTree');
-// var NancyFamily = require('../src/NancyFamily');
+var NancyFamily = require('../src/NancyFamily');
+// var Nancy = new FamilyTree('Nancy');
+// var NancyFamilyTree = Nancy.build(NancyFamily);
 
 describe('FamilyTree class', function() {
 
-  var family = new FamilyTree('Aaron');
+  var Aaron = new FamilyTree('Aaron');
 
   describe('constructors', function(){
 
     it('should have a name property', function(){
-      expect(family.hasOwnProperty('name')).toEqual(true);
+      expect(Aaron.hasOwnProperty('name')).toEqual(true);
     });
 
     it('should have a children property', function(){
-      expect(family.hasOwnProperty('children')).toEqual(true);
+      expect(Aaron.hasOwnProperty('children')).toEqual(true);
     });
 
     describe('name property', function(){
 
       it('should be initialized to passed in name argument', function(){
-        expect(family.name).toEqual('Aaron');
+        expect(Aaron.name).toEqual('Aaron');
       });
     });
 
     describe('children property', function(){
 
       it('should be initialized an empty array', function(){
-        expect(family.children.length).toEqual(0);
+        expect(Aaron.children.length).toEqual(0);
       });
     });
+  });
+
+  describe('class methods', function(){
+
+    var Bob = new FamilyTree('Bob');
+    var Bill = new FamilyTree('Bill');
+
+    describe('addChild function', function(){
+
+      it('should exist on FamilyTree prototype', function(){
+        expect(FamilyTree.prototype.hasOwnProperty('addChild')).toEqual(true);
+      });
+
+      it('should add a new child to the children array', function(){
+        Bob.addChild(Bill);
+        expect(Bob.children.length).toEqual(1);
+        expect(Bob.children[0]).toEqual(Bill);
+      });
+    });
+
+    describe('build function', function(){
+
+      it('should exist on FamilyTree prototype', function(){
+        expect(FamilyTree.prototype.hasOwnProperty('build')).toEqual(true);
+      });
+
+      it('should take a json object and build a family tree from it', function(){
+
+        var Bobby = new FamilyTree('Bobby');
+        var json = {
+          'Bob': {
+            'Bill': null,
+            'Byron': {
+              'Bo': null
+            }
+          }
+        };
+
+        Bobby.build(json);
+        expect(Bobby.children.length).toEqual(2);
+        expect(Bobby.children[1].name).toEqual('Byron');
+        expect(Bobby.children[1].children[0]).toEqual(new FamilyTree('Bo'));
+      });
+    });
+
+    describe('traverse function', function(){
+
+      xit('should exist on FamilyTree prototype', function(){
+        expect(FamilyTree.prototype.hasOwnProperty('traverse')).toEqual(true);
+      });
+
+      xit('should traverse entire family tree and apply an iterator to each member', function(){
+
+      });
+
+    });
+
+    describe('filter function', function(){
+
+      xit('should exist on FamilyTree prototype', function(){
+        expect(FamilyTree.prototype.hasOwnProperty('filter')).toEqual(true);
+      });
+
+      xit('should return a collection of members that meet a condition', function(){
+
+      });
+
+    });
+
+    describe('find function', function(){
+
+      xit('should exist on FamilyTree prototype', function(){
+        expect(FamilyTree.prototype.hasOwnProperty('find')).toEqual(true);
+      });
+
+      xit('should find the member in the family tree given the name', function(){
+
+      });
+
+    });
+
   });
 
 });
--- src/FamilyTree.js
+++ src/FamilyTree.js
@@ -5,4 +5,38 @@ var FamilyTree = function(name){
   this.children = [];
 };
 
+FamilyTree.prototype.addChild = function(child){
+  this.children.push(child);
+};
+
+FamilyTree.prototype.build = function(json){
+
+  var children = json[this.name];
+  var descendents;
+
+  if (!children) {
+    return this;
+  }
+
+  for (var child in children) {
+    child = new FamilyTree(child);
+    descendents = child.build(children);
+    this.addChild(descendents);
+  }
+
+  return this;
+};
+
+FamilyTree.prototype.traverse = function(iterator){
+
+};
+
+FamilyTree.prototype.filter = function(condition){
+
+};
+
+FamilyTree.prototype.find = function(name){
+
+};
+
 module.exports = FamilyTree;
